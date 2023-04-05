a7c012f9d69cd87325dbe9801824cd511b090aaf
andre <ank2.qc@gmail.com>
2023-04-05 17:05:53 -0300

    final

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,29 +1,30 @@
 #get the ascii number for the letter, change to number sum/subtract, change back to letters
 # .ord letter to number, .char number to letter
 # 65 A, 90 Z, 97 a, 122 z
+# r % n, r-n*(r/n).floor, r.divmod(n)[1]
 
-def ceasar_cipher(word, number)
-    number 
+def caesar_cipher(word, number)
+    number = number % 26
     letters = word.split("")
     letters.map! do |letter|
         number_letter = letter.ord
-        if number_letter >= 65 && number_letter <= 90
-            initial = 65
-            final = 90
-        elsif number_letter >= 97 && number_letter <= 122
-            initial = 97
-            final = 122
+        if number_letter.between?(65, 90) || number_letter.between?(97, 122)
+            if number_letter.between?(65, 90)
+                initial = 65
+                final = 90
+            elsif number_letter.between?(97, 122)
+                initial = 97
+                final = 122
+            end
+            number_letter = number_letter + number
+            number_letter = (number_letter - final) + initial - 1 if number_letter > final
         end
-
-        new_number = number_letter + number
-        new_number = (new_number % final) + initial if new_number > final
-
-        puts new_number
-
-        letter = new_number.chr
+        letter = number_letter.chr
     end
-    new_letters = letters.join("")
-    puts new_letters
+    puts letters.join("")
 end
 
-ceasar_cipher("tesetzz", 3)
+caesar_cipher("What a string!", 5)
+caesar_cipher("What a string!", -5)
+caesar_cipher("What a string!", 50)
+caesar_cipher("What a string!", -50)
